d2c7dd0a7271c12222287ac914b7fd5b89ef3149
Chan-Hee <lch951022@gmial.com>
2017-10-21 14:49:05 +0900

    mkdir

--- Code/Python/DNN_cancer_conf.py
+++ Code/Python/DNN_cancer_conf.py
@@ -6,13 +6,31 @@ import math
 import pandas as pd
 
 ####Set Random Seed ####
-tf.set_random_seed(777)  
+tf.set_random_seed(777) 
+
+def cal_var(variances, per):
+    all_cnt = len(variances)
+    per = 100-per
+    per_idx = int(all_cnt*(per/100))
+    return variances[per_idx]
+    
+def five_fold(data, num):
+    test_set = data[num*2000:(num+1)*2000]
+    train_set = data[:num*2000]
+    train_set = np.concatenate((train_set,data[(num+1)*2000:] ), axis=0)
+    return train_set , test_set
+    
+def top_of_variance(per,  data_x):
+    ##data_x['variance']
+    ##calculate value  
+    data = data_x[data_x['VAR'] > per]
+    idx_list = data.index.tolist()
+    ##return index 
+    return idx_list
 
 def set_train_three_layer(num,repeat, nodes, learning_rate):
-    print(num)
     train_a = 0
     test_a = 0
-    repeat = 1
     X = tf.placeholder(tf.float32, [None, cnt_train])
     Y = tf.placeholder(tf.float32, [None, 1])
 
@@ -50,6 +68,9 @@ def set_train_three_layer(num,repeat, nodes, learning_rate):
                 ####Train Accuracy report####
                 h, c, train_a = sess.run([hypothesis, predicted, accuracy],feed_dict={X: train_x, Y: train_y})
                 print("\nTrain Accuracy: ", train_a)
+            if step % 2000 == 0 : 
+                h, c, train_a = sess.run([hypothesis, predicted, accuracy],feed_dict={X: train_x, Y: train_y})
+                print("\nCurrent Accuracy : ", train_a , "Current Step : ", step)
         ######Accuracy Report#####
         h, c, test_a = sess.run([hypothesis, predicted, accuracy],feed_dict={X: test_x, Y: test_y})    
         print("\nTest Accuracy: ", test_a)
@@ -57,10 +78,8 @@ def set_train_three_layer(num,repeat, nodes, learning_rate):
     return train_a, test_a
         
 def set_train_four_layer(num ,repeat, nodes, learning_rate):
-    print(num)
     train_a = 0
     test_a = 0
-    repeat = 1
     X = tf.placeholder(tf.float32, [None, cnt_train])
     Y = tf.placeholder(tf.float32, [None, 1])
 
@@ -102,52 +121,74 @@ def set_train_four_layer(num ,repeat, nodes, learning_rate):
                 ####Train Accuracy report####
                 h, c, train_a = sess.run([hypothesis, predicted, accuracy],feed_dict={X: train_x, Y: train_y})
                 print("\nTrain Accuracy: ", train_a)
+            if step % 2000 == 0 : 
+                h, c, train_a = sess.run([hypothesis, predicted, accuracy],feed_dict={X: train_x, Y: train_y})
+                print("\nCurrent Accuracy : ", train_a , "Current Step : ", step)
         ######Accuracy Report#####
         h, c, test_a = sess.run([hypothesis, predicted, accuracy],feed_dict={X: test_x, Y: test_y})    
         print("\nTest Accuracy: ", test_a)
     
     return train_a, test_a
-        
 
 ####Read data####
-xdata = np.genfromtxt ('/home/tjahn/Data/DNNData/DNNToyVar1.csv', delimiter=",",usecols=np.arange(0,2007))
-ydata = np.genfromtxt ('/home/tjahn/Data/DNNData/CancerResult.csv', delimiter=",")
-xdata = xdata[1:, 3:-1]  # eliminate heading, string data, variance
-ydata = ydata[1:, 1:]    # eliminate heading, string data
-xdata = xdata.transpose()
-
-####Data Processing - divide train and test set####
-train_x = xdata[:1000]
-train_y = ydata[:1000]
-test_x = xdata[1000:]
-test_y = ydata[1000:]
-
-cnt_train = len(train_x[1, :])
-
-####Read train configuration
-filename = input("Insert configuration filename : ")
-conf = pd.read_csv(filename)
+#x_filename = input("Insert X dataset directory and name  : ")
+#y_filename = input("Insert Y dataset directory and name : ")
+x_filename = '/home/tjahn/Data/DNN10000/DNN10000.csv'
+xdata = pd.read_csv(x_filename)
+ydata = np.genfromtxt('/home/tjahn/Data/DNN10000/CancerResult.csv', delimiter=",")
+#conf_filename = input("Insert configure file directory and name : ")
+conf_filename = '/home/tjahn/learning_ps.csv'
+conf = pd.read_csv(conf_filename)
 print(conf)
-
 train_accs = []
 test_accs = []
 for i in range(len(conf)):
-    train_acc = 0
-    test_acc = 0
-    if(conf.iloc[i]['layer'] == 3):
-        repeat, layer, node , learning_rate, gene = conf.iloc[i]
-        nodes = list(map(int , node.split(" ")))
-        train_acc , test_acc = (set_train_three_layer(i,repeat, nodes, learning_rate))
-        train_accs.append(train_acc)
-        test_accs.append(test_acc)
-    elif(conf.iloc[i]['layer']== 4):
-        repeat, layer, node , learning_rate, gene = conf.iloc[i]
-        nodes = list(map(int , node.split(" ")))
-        train_acc , test_acc = (set_train_four_layer(i,repeat, nodes, learning_rate))
-        train_accs.append(train_acc)
-        test_accs.append(test_acc)
+    repeat, layer, node , learning_rate, gene = conf.iloc[i]
+    nodes = list(map(int , node.split(" ")))
+    train_accs_conf = []
+    test_accs_conf = []
+    for j in range(5):
+        variance_set = pd.concat([xdata.iloc[:2000*j], xdata.iloc[2000*(j+1):]])
+        #print(variance_set.iloc[:,-1])
+        variances = variance_set.iloc[:,-1]
+        #print(variances)
+        variances = variances.as_matrix()
+        #print(variances)
+        variances = np.sort(variances)
+        #print(variances)
+        idx = top_of_variance(cal_var(variances, gene) , variance_set)
+        data_x = xdata.loc[idx]
+        data_x = data_x.as_matrix()
+        data_x = data_x[1:, 3:-1]
+        data_y = ydata[1:, 1:]    # eliminate heading, string data
+        data_x = data_x.transpose()
+        ###5-fold data 
+        ####Data Processing - divide train and test set####
+        ####5-fold code  is needed ####
+        print(len(data_x), len(data_y))
+        train_x, test_x = five_fold(data_x,j)
+        train_y, test_y = five_fold(data_y,j)
+        #print(train_y)
+        cnt_train = len(train_x[1, :])
+        if(conf.iloc[i]['layer'] == 3):
+            train_acc , test_acc = (set_train_three_layer(i,repeat, nodes, learning_rate))
+            train_accs_conf.append(train_acc)
+            test_accs_conf.append(test_acc)
+        elif(conf.iloc[i]['layer']== 4):
+            train_acc , test_acc = (set_train_four_layer(i,repeat, nodes, learning_rate))
+            train_accs_conf.append(train_acc)
+            test_accs_conf.append(test_acc)
+    train_accs.append(train_accs_conf)
+    test_accs.append(test_accs_conf)
 
-conf['train_acc'] = train_accs
-conf['test_acc'] = test_accs
 
-conf.to_csv('/Home/tjahn/Result.csv')
+train_accs = pd.DataFrame(data=train_accs , 
+                          index = list(range(len(conf))) , 
+                          columns = ["tr-fold-1","tr-fold-2","tr-fold-3","tr-fold-4","tr-fold-5"])
+test_accs = pd.DataFrame(data=test_accs , 
+                          index = list(range(len(conf))) , 
+                          columns = ["te-fold-1","te-fold-2","te-fold-3","te-fold-4","te-fold-5"])
+
+accuracies = pd.concat([train_accs, test_accs], axis=1)
+conf = pd.concat([conf, accuracies] , axis = 1)
+conf.to_csv(conf_filename[:-4]+'result.csv')
