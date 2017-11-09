8ed93c821af229ac0dc2cece99ec91377e232d27
Chan-Hee <lch951022@gmail.com>
2017-11-09 12:06:59 +0900

    step-wise

--- User/chanhee/logistic_model.R
+++ User/chanhee/logistic_model.R
@@ -16,54 +16,64 @@ data$result<-result[,2]
 # Random Sampling -> Test & Training Data set
 num_of_samples <- dim(data)[1]
 
-rand_index <- sample(1:num_of_samples,num_of_samples)
-test_index <- rand_index[1:as.integer(num_of_samples*0.2)]
-train_index <- rand_index[as.integer(num_of_samples*0.2+1):num_of_samples]
+##rand_index <- sample(1:num_of_samples,num_of_samples)
+#test_index <- rand_index[1:as.integer(num_of_samples*0.2)]
+##train_index <- rand_index[as.integer(num_of_samples*0.2+1):num_of_samples]
+##train<-data[train_index,]
+##test<-data[test_index,]
 
-train<-data[train_index,]
-test<-data[test_index,]
-
-# Model Fitting
-results <- names(data) %in% "result"
-genes <- colnames(data[,!results])
-
-num_of_attributes<-dim(data)[2]-1
-betas <- c()
-
-num_of_attributes = 10
-
-for(i in 1:num_of_attributes){
-  glm_model<-glm(result~get(genes[i]),family = binomial(link = logit),data = train)
-
-  beta<-abs(glm_model$coefficients[2])
-  names(beta)<-genes[i]
+# 5-fold cross validation
+k = 5
+data$id<-sample(1:k,nrow(data),replace = TRUE)
+list = 1:k
+reports=data.frame()
+for(i in 1:k){
+  train<-subset(data, id %in% list[-i])
+  test<-subset(data, id %in% c(i))
+  results <- names(data) %in% "result"
   
-  betas<-c(betas,beta)
-}
+  num_of_attributes<-dim(data)[2]-2
+  genes <- colnames(data[,!results])
+  betas <- c()
+  
+  #num_of_attributes = 10
+  
+  for(i in 1:num_of_attributes){
+    glm_model<-glm(result~get(genes[i]),family = binomial(link = logit),data = train)
+    
+    beta<-abs(glm_model$coefficients[2])
+    names(beta)<-genes[i]
+    
+    betas<-c(betas,beta)
+  }
+  
+  betas <- sort(betas,decreasing = TRUE)  
+  betas30<- betas[1:as.integer(0.3*num_of_attributes)]
+  
+  # Model Fitting
+  y<-"result"
+  x<-names(betas30)
+  flma<-paste(y,paste(x,collapse ="+"),sep = "~")
+  
+  final_model<-glm(flma,family = binomial(link = logit),data = train)
+  model.aic.forward<-step(final_model,direction = "forward")
+  
+  train_predict<-predict(model.aic.forward,newdata = train,type = "response")
+  train_predict<-ifelse(train_predict>0.5,1,0)
+  train_tble<-table(train_predict,actual.class = train$result)
+  train_accuracy<-(train_tble[1,1]+train_tble[2,2])/sum(train_tble)
+  
+  test_predict<-predict(model.aic.forward,newdata = test,type = "response")
+  test_predict<-ifelse(test_predict>0.5,1,0)
+  test_tble<-table(test_predict,actual.class = test$result)
+  test_accuracy<-(test_tble[1,1]+test_tble[2,2])/sum(test_tble)
+  
+  report<-data.frame(test = test_accuracy, train = train_accuracy)
+  reports<-rbind(reports,report)
 
-betas <- sort(betas,decreasing = TRUE)  
-betas30<- betas[1:as.integer(0.3*num_of_attributes)]
-
-
-y<-"result"
-x<-names(betas30)
-flma<-paste(y,paste(x,collapse ="+"),sep = "~")
-
-final_model<-glm(flma,family = binomial(link = logit),data = train)
-
-train_predict<-predict(final_model,newdata = train,type = "response")
-train_predict<-ifelse(train_predict>0.5,1,0)
-train_tble<-table(train_predict,actual.class = train$result)
-
-train_accuracy<-(train_tble[1,1]+train_tble[2,2])/sum(train_tble)
-
-test_predict<-predict(final_model,newdata = test,type = "response")
-test_predict<-ifelse(test_predict>0.5,1,0)
-test_tble<-table(test_predict,actual.class = test$result)
-test_accuracy<-(test_tble[1,1]+test_tble[2,2])/sum(test_tble)
-
-report<-data.frame(test = test_accuracy, train = train_accuracy)
+  
+  
+  }
 
 setwd("/home/tjahn/Git/User/chanhee")
-write.csv(report,"logistic_result.csv")
-
+write.csv(report,"stepwids_fivefold_logistic_result.csv")
