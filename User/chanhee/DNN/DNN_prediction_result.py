4d1c6170594d44e7acf7021bb4f205b990e8ea44
Chan-Hee <lch951022@gmail.com>
2017-12-26 16:49:09 +0900

    calibration

--- User/chanhee/DNN/DNN_prediction_result.py
+++ User/chanhee/DNN/DNN_prediction_result.py
@@ -88,7 +88,7 @@ def set_train_three_layer(repeat, nodes, learning_rate):
                     print("Number of Calibration:",len(calibration_queue))
 
                 cal_sum=sum(calibration_queue)/float(len(calibration_queue))
-                if abs(cal_sum-max(calibration_queue))/cal_sum<0.01 and abs(cal_sum-min(calibration_queue))/cal_sum<0.01:
+                if abs(cal_sum-max(calibration_queue))/cal_sum<0.01 and abs(cal_sum-min(calibration_queue))/cal_sum<0.01 and len(calibration_queue)==5:
                     print("cal_a:",cal_a)
                     print("BREAK!!")
                     break
