1aabf603c27ba6e2935b0d8fcc6d36ab3a0b3b8b
Chan-Hee <lch951022@gmial.com>
2018-01-19 13:11:55 +0900

    sm

--- User/sungmin/DNN/DNN_modifying_inputfile.py
+++ User/sungmin/DNN/DNN_modifying_inputfile.py
@@ -4,7 +4,6 @@ import numpy as np
 import math
 import pandas as pd
 import tensorflow as tf
-import random
 from scipy import stats
 
 
@@ -174,7 +173,7 @@ for i in range(len(conf)):
     for j in range(5):
     #####Five fold#####
         train_data, test_data = five_fold(data, j)
-        random.shuffle(test_data)
+        test_data.sample(frac = 1 )
         cal_data = test_data[:int(len(test_data)/2)]
         test_data = test_data[int(len(test_data)/2):]
         train_GSM = train_data.iloc[:,0].tolist()
