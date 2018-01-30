8514ab61620d992254613e6b9a873cd7f9325419
Chan-Hee <lch951022@gmial.com>
2018-01-30 13:07:39 +0900

    sm

--- User/sungmin/DNN/DNN_1.py
+++ User/sungmin/DNN/DNN_1.py
@@ -1,11 +1,4 @@
-
-# coding: utf-8
-
 # ##################Import Modules#######################
-
-# In[1]:
-
-
 import random
 import numpy as np
 import math
@@ -15,18 +8,9 @@ from scipy import stats
 
 tf.set_random_seed(777)
 
-
 # ##################Define Functions#####################
-
-# In[13]:
-
-
 def five_fold_name(data,i):
     test_names = data[data['index']==i+1]
-
-
-# In[14]:
-
 
 def five_fold(data, i):
     test_data = data[data['index']==i+1]
@@ -35,132 +19,9 @@ def five_fold(data, i):
 
     return train_data , test_data
 
-
-# In[24]:
-
-
-def set_train_three_layer(nodes, learning_rate, j, gene_off):
-    batch_size = 1000
-    tf.reset_default_graph()
-    keep_prob = tf.placeholder(tf.float32)
-
-    X = tf.placeholder(tf.float32, [None, cnt_train])
-    Y = tf.placeholder(tf.float32, [None, 2])
-
-    W1 = tf.get_variable( shape= [cnt_train, nodes[0]], name='weight1' , initializer=tf.contrib.layers.xavier_initializer())
-    b1 = tf.Variable(tf.random_normal([nodes[0]]), name='bias1')
-    layer1 = tf.nn.relu(tf.matmul(X, W1) + b1)
-    layer1 = tf.nn.dropout(layer1, keep_prob=keep_prob)
-
-    W2 = tf.get_variable(shape =[nodes[0], nodes[1]], name='weight2', initializer=tf.contrib.layers.xavier_initializer())
-    b2 = tf.Variable(tf.random_normal([nodes[1]]), name='bias2')
-    layer2 = tf.nn.relu(tf.matmul(layer1, W2) + b2)
-    layer2 = tf.nn.dropout(layer2 , keep_prob=keep_prob)
-
-    W3 = tf.get_variable(shape= [nodes[1], nodes[2]], name='weight3',initializer=tf.contrib.layers.xavier_initializer())
-    b3 = tf.Variable(tf.random_normal([nodes[2]]), name='bias3')
-    layer3 = tf.nn.relu(tf.matmul(layer2, W3) + b3)
-    layer3 = tf.nn.dropout(layer3, keep_prob=keep_prob)
-
-    W4 = tf.get_variable(shape=[nodes[2], 2], name='weight4',initializer=tf.contrib.layers.xavier_initializer())
-    b4 = tf.Variable(tf.random_normal([2]), name='bias4')
-    hypothesis = tf.matmul(layer3, W4) + b4
-
-
-
-    # cost/loss function
-    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=hypothesis, labels=Y))
-    train = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
-    cost_summ = tf.summary.scalar(str(int(gene_off))+"_"+str(nodes)+"_cost",cost)
-
-
-
-    # Accuracy computation
-    # True if hypothesis>0.5 else False
-
-    predicted = tf.argmax(hypothesis,1)
-    correct_prediction = tf.equal(predicted,tf.argmax(Y,1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, dtype=tf.float32))
-    accuracy_summ = tf.summary.scalar(str(int(gene_off))+"_"+str(nodes)+"_accuracy",accuracy)
-
-    saver = tf.train.Saver()
-    with tf.Session() as sess:
-        merged_summary = tf.summary.merge_all()
-        writer = tf.summary.FileWriter(tensorboard_directory+"with_rare_cancer/gene_off_"+str(int(gene_off)) +"/" + str(nodes) +"_" + str(j+1))
-        writer.add_graph(sess.graph)
-
-        # Initialize TensorFlow variables
-        sess.run(tf.global_variables_initializer())
-        stop_switch = True
-        step=0
-        AccuracyList=[]
-        
-        max_step = 0
-        max_Accuracy = 0
-
-        while(stop_switch):
-            total_num = int(len(train_x)/batch_size)
-            for i in range(total_num):
-                batch_x = train_x[i*batch_size:(i+1)*batch_size]
-                batch_y = train_y[i*batch_size:(i+1)*batch_size]
-                sess.run(train , feed_dict={X: batch_x, Y: batch_y , keep_prob : 1})
-                summary,_=sess.run([merged_summary,train], feed_dict={X: batch_x, Y: batch_y , keep_prob : 1})
-                writer.add_summary(summary, global_step =step)
-
-            train_h,c, train_p,train_a = sess.run([hypothesis, cost ,predicted, accuracy],feed_dict={X: train_x, Y: train_y, keep_prob :1})
-            cal_h,c, cal_p,cal_a = sess.run([hypothesis, cost ,predicted, accuracy],feed_dict={X: cal_x, Y: cal_y, keep_prob :1})
-            step+=1
-            print("\nTraining Accuracy : ", train_a , "Calibration Accuracy : ", cal_a,"Step :", step)
-            if len(AccuracyList) == 20:
-                AccuracyList.pop(0)
-                AccuracyList.append(cal_a)
-                beforeAccuracy = AccuracyList[:int(len(AccuracyList)/2)]
-                afterAccuracy = AccuracyList[int(len(AccuracyList)/2):]
-                tTestResult = stats.ttest_rel(beforeAccuracy,afterAccuracy)
-                print("P-Value: ",tTestResult.pvalue,"\n",beforeAccuracy,"\n",afterAccuracy)
-
-#save path
-                if(max(AccuracyList)> max_Accuracy):
-                    max_step = step
-                    max_Accuracy = max(AccuracyList)
-                    save_path = saver.save(sess, save_path_directory)
-                    print("Save path: ",save_path,"\nMax_step: ",max_step,"\nMax_Accuracy: ",max_Accuracy )
-                 
-#Early stopping
-                if max(AccuracyList)-min(AccuracyList)< 0.01 and min(AccuracyList)>0.94 and max(beforeAccuracy) >= max(afterAccuracy) :
-                    stop_switch = False
-                    print("Learning Finished!! P-Value: ",tTestResult.pvalue,"\n",beforeAccuracy,"\n",afterAccuracy)
-            else:
-                AccuracyList.append(cal_a)
-
-
-
-        #w1_matrix=W1.eval()
-
-        #weighted_sum = w1_matrix.sum(axis=1)
-        #weighted_max = w1_matrix.max(axis=1)
-        #gene_names = list(data)[1:-2]
-
-        saver.restore(sess,save_path)
-        print("Max_step: ",max_step,"Max_accuracy: ", max_Accuracy)
-       # print("W1, W2, W3, W4: "+ W1.eval() + " "+ W2.eval()+ " " +W3.eval() + " " + W4.eval())
-      #  print("W1, W2, W3, W4: %s  %s  %s  %s \n", W1.eval(),W2.eval(),W3.eval(), W4.eval())
-
-        test_h, test_p, test_a = sess.run([hypothesis, predicted, accuracy],feed_dict={X: test_x, Y: test_y, keep_prob :1.0})
-        print("\nTest Accuracy: ", test_a)
-
-        weighted_sum_result = [str(int(gene_off)),train_a,cal_a,test_a]
-    #    weighted_sum_result = [gene_off,train_a,cal_a,test_a,max_step,max_Accuracy]
-         
-    return train_p ,train_h, test_p,test_h,weighted_sum_result
-
-
-# In[6]:
-
-
 def sm_deep_learning(layer, nodes, learning_rate, five_fold_count, gene_off):
     
-####message for stsrt
+####message for start
     print("Gene_off: ",gene_off,"\nLayer: ",layer,"\nNodes: ",nodes,"\n Five fold count: ",five_fold_count+1)
     batch_size = 1000
     tf.reset_default_graph()
@@ -208,7 +69,7 @@ def sm_deep_learning(layer, nodes, learning_rate, five_fold_count, gene_off):
     saver = tf.train.Saver()
     with tf.Session() as sess:
         merged_summary = tf.summary.merge_all()
-        writer = tf.summary.FileWriter(tensorboard_directory+"with_rare_cancer/gene_off_"+str(int(gene_off)) +"/" + str(layer) +"/" + str(nodes) +"_" + str(five_fold_count+1))
+        writer = tf.summary.FileWriter(tensorboard_directory+"gene_off_"+str(int(gene_off)) +"/" + str(layer) +"/" + str(nodes) +"_" + str(five_fold_count+1))
         writer.add_graph(sess.graph)
 
         # Initialize TensorFlow variables
@@ -283,12 +144,12 @@ def sm_deep_learning(layer, nodes, learning_rate, five_fold_count, gene_off):
 
 
 learning_rate = 0.002
-
-output_directory = "/home/tjahn/tf_save_data/sungmin/result/with_rare_cancer/"
+concept_directory = "pre/"
+output_directory = "/home/tjahn/tf_save_data/sungmin/result/"+concept_directory
 conf_directory = "/home/tjahn/Git2/User/sungmin/DNN/input/"
 data_directory = "/home/tjahn/Data/"
-tensorboard_directory = "/home/tjahn/tf_save_data/sungmin/tensorboard/"
-save_path_directory = "/home/tjahn/tf_save_data/sungmin/save_path/with_rare_cancer/saved/"
+tensorboard_directory = "/home/tjahn/tf_save_data/sungmin/tensorboard/"+concept_directory
+save_path_directory = "/home/tjahn/tf_save_data/sungmin/save_path/saved/"+concept_directory
 ####input = layer node gene_selection 
 conf_filename = "input.csv"
 conf = pd.read_csv(conf_directory + conf_filename)
@@ -306,7 +167,7 @@ for i in range(len(conf)):
    
 
 ####sm
-    datafilename = "FinalData_Random6000_Random_"+str(gene_off)+"off_GSM_gene_index_result.csv"
+    datafilename = "FinalData"+str(gene_off)+"off_GSM_gene_index_result.csv"
     data = pd.read_csv(data_directory + datafilename)
 ####sm
     Gene_Elimination = []
@@ -346,7 +207,7 @@ for i in range(len(conf)):
         cal_y = pd.get_dummies(cal_y)
 
 
-        train_p, train_h , test_p ,test_h, weighted_sum_result  = (set_train_three_layer(nodes, learning_rate, j, gene_off))
+        #train_p, train_h , test_p ,test_h, weighted_sum_result  = (set_train_three_layer(nodes, learning_rate, j, gene_off))
         train_p, train_h, test_p, test_h, weighted_sum_result = sm_deep_learning(layer, nodes, learning_rate, j, gene_off)
         train_p = pd.DataFrame(train_p, index = train_GSM )
         train_h = pd.DataFrame(train_h , index = train_GSM)
