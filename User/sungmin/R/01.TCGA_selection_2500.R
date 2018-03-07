bce8e30f86d5b7dce13935717c3d17c98af0c9d6
ksmpooh <daverpooh@naver.com>
2018-03-07 17:10:21 +0900

    sm

--- User/sungmin/R/01.TCGA_selection_2500.R
+++ User/sungmin/R/01.TCGA_selection_2500.R
@@ -18,7 +18,7 @@ VAR <- GetVar(genes)
 genes <- rbind(genes,VAR)
 genes <- genes[,rev(order(genes[nrow(genes),]))]
 genes <- genes[-nrow(genes),1:2500]
-df_ch <- data.frame(patients, genes, result, cancer_code, index)
+df_ch <- cbind(patients, genes, result, cancer_code, index)
 
 write.csv(df_ch, "/home/tjahn/Data/sungminTCGAtoGEO_2500/inter_wt_cancer_code_Final_TCGA_gene_expression_FPKM_2500.csv", row.names=FALSE)
 write.csv(names(genes), "/home/tjahn/Data/sungminTCGAtoGEO_2500/TCGA_FPKM_2500_names.csv", row.names=FALSE)
@@ -36,7 +36,7 @@ VAR <- GetVar(genes)
 genes <- rbind(genes,VAR)
 genes <- genes[,rev(order(genes[nrow(genes),]))]
 genes <- genes[-nrow(genes),1:2500]
-df_ch <- data.frame(patients, genes, result, cancer_code, index)
+df_ch <- cbind(patients, genes, result, cancer_code, index)
 
 write.csv(df_ch, "/home/tjahn/Data/sungminTCGAtoGEO_2500/inter_wt_cancer_code_Final_TCGA_gene_expression_FPKM-UQ_2500.csv", row.names=FALSE)
 write.csv(names(genes), "/home/tjahn/Data/sungminTCGAtoGEO_2500/TCGA_FPKM-UQ_2500_names.csv", row.names=FALSE)
@@ -53,7 +53,7 @@ VAR <- GetVar(genes)
 genes <- rbind(genes,VAR)
 genes <- genes[,rev(order(genes[nrow(genes),]))]
 genes <- genes[-nrow(genes),1:2500]
-df_ch <- data.frame(patients, genes, result, cancer_code, index)
+df_ch <- cbind(patients, genes, result, cancer_code, index)
 
 write.csv(df_ch, "/home/tjahn/Data/sungminTCGAtoGEO_2500/TCGA_htseq_2500_names.csv", row.names=FALSE)
 write.csv(names(genes), "GEO_2500_names.csv", row.names=FALSE)
