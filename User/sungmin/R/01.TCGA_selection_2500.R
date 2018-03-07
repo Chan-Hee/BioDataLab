c956155191d9e96601146a0cb103bfa4932bd6c3
Chan-Hee <lch951022@gmial.com>
2018-03-07 17:08:52 +0900

    sm

--- User/sungmin/R/01.TCGA_selection_2500.R
+++ User/sungmin/R/01.TCGA_selection_2500.R
@@ -9,7 +9,7 @@ GetVar<-function(genes){
 df<-read.csv("/home/tjahn/Data/01.4964/inter_wt_cancer_code_Final_TCGA_gene_expression_FPKM_4964.csv")
 names(df) <- gsub(".", "-", names(df), fixed = TRUE)
 genes <- df[,2:(ncol(df)-3)]
-patients <- df$patient
+patients <- df$patients
 result <- df$result
 cancer_code <- df$cancer_code
 index <- df$index
@@ -18,7 +18,7 @@ VAR <- GetVar(genes)
 genes <- rbind(genes,VAR)
 genes <- genes[,rev(order(genes[nrow(genes),]))]
 genes <- genes[-nrow(genes),1:2500]
-df_ch <- cbind(patient, genes, result, cancer_code, index)
+df_ch <- data.frame(patients, genes, result, cancer_code, index)
 
 write.csv(df_ch, "/home/tjahn/Data/sungminTCGAtoGEO_2500/inter_wt_cancer_code_Final_TCGA_gene_expression_FPKM_2500.csv", row.names=FALSE)
 write.csv(names(genes), "/home/tjahn/Data/sungminTCGAtoGEO_2500/TCGA_FPKM_2500_names.csv", row.names=FALSE)
@@ -27,7 +27,7 @@ write.csv(names(genes), "/home/tjahn/Data/sungminTCGAtoGEO_2500/TCGA_FPKM_2500_n
 df<-read.csv("/home/tjahn/Data/01.4964/inter_wt_cancer_code_Final_TCGA_gene_expression_FPKM-UQ_4964.csv")
 names(df) <- gsub(".", "-", names(df), fixed = TRUE)
 genes <- df[,2:(ncol(df)-3)]
-patients <- df$patient
+patients <- df$patients
 result <- df$result
 cancer_code <- df$cancer_code
 index <- df$index
@@ -36,7 +36,7 @@ VAR <- GetVar(genes)
 genes <- rbind(genes,VAR)
 genes <- genes[,rev(order(genes[nrow(genes),]))]
 genes <- genes[-nrow(genes),1:2500]
-df_ch <- cbind(patient, genes, result, cancer_code, index)
+df_ch <- data.frame(patients, genes, result, cancer_code, index)
 
 write.csv(df_ch, "/home/tjahn/Data/sungminTCGAtoGEO_2500/inter_wt_cancer_code_Final_TCGA_gene_expression_FPKM-UQ_2500.csv", row.names=FALSE)
 write.csv(names(genes), "/home/tjahn/Data/sungminTCGAtoGEO_2500/TCGA_FPKM-UQ_2500_names.csv", row.names=FALSE)
@@ -44,7 +44,7 @@ write.csv(names(genes), "/home/tjahn/Data/sungminTCGAtoGEO_2500/TCGA_FPKM-UQ_250
 df<-read.csv("/home/tjahn/Data/01.4964/inter_wt_cancer_code_Final_TCGA_gene_expression_htseq_4964.csv")
 names(df) <- gsub(".", "-", names(df), fixed = TRUE)
 genes <- df[,2:(ncol(df)-3)]
-patients <- df$patient
+patients <- df$patients
 result <- df$result
 cancer_code <- df$cancer_code
 index <- df$index
@@ -53,7 +53,7 @@ VAR <- GetVar(genes)
 genes <- rbind(genes,VAR)
 genes <- genes[,rev(order(genes[nrow(genes),]))]
 genes <- genes[-nrow(genes),1:2500]
-df_ch <- cbind(patient, genes, result, cancer_code, index)
+df_ch <- data.frame(patients, genes, result, cancer_code, index)
 
 write.csv(df_ch, "/home/tjahn/Data/sungminTCGAtoGEO_2500/TCGA_htseq_2500_names.csv", row.names=FALSE)
 write.csv(names(genes), "GEO_2500_names.csv", row.names=FALSE)
