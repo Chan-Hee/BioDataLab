ab905180890e2eb4d4334d36ed72bd10525680e7
ksmpooh <daverpooh@naver.com>
2018-04-04 18:26:05 +0900

    sm

--- User/sungmin/R/04.To_find_best_model_in_113_models.R
+++ User/sungmin/R/04.To_find_best_model_in_113_models.R
@@ -10,7 +10,7 @@ for (i in 0:4){
   correct = 0
   for(j in 3:ncol(df)){
     for(k in 1:nrow(df)){
-      if(df[k,j] == df[k,df$result]){
+      if(df[k,j] == df[k,"result"]){
         correct = correct + 1
       }else{
         correct = correct + 0
@@ -24,4 +24,15 @@ for (i in 0:4){
 }
 
 #df = read.csv(paste0("D:/biodatalab/2018-1/prediction_output_each_GEO_in_each_model/prediction_output",i,"_data.csv"),sep = ",",header = T)
-
+#df <- cbind(df[,2:3],round(subset(df,select = -c(1:3)),0))
+#correct = 0
+#for(j in 3:ncol(df)){
+#  for(k in 1:nrow(df)){
+#    if(df[k,j] == df[k,"result"]){
+#      correct = correct + 1
+#    }else{
+#      correct = correct + 0
+#    }
+#  }
+#  df[(nrow(df)+1),j] <- nrow(df)/correct
+#}
