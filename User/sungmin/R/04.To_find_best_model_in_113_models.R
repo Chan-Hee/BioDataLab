40d54b800a1c425088a99df977764d72a6622b3a
ksmpooh <daverpooh@naver.com>
2018-04-04 10:55:53 +0900

    sm

--- User/sungmin/R/04.To_find_best_model_in_113_models.R
+++ User/sungmin/R/04.To_find_best_model_in_113_models.R
@@ -4,7 +4,9 @@ for (i in 0:4){
   df = read.csv(paste0("/home/tjahn/FINALMODEL/BioDataLab_ML/prediction_output/prediction_output",i,"_data.csv"),sep = ",",header = T)
   #df_prediction = round(subset(df,select = -c(1:3)),0)
   df = cbind(df[,2:3],round(subset(df,select = -c(1:3)),0))
-  object = nrow(df)
+  #object = nrow(df)
+  k = 0
+  j = 3
   correct = 0
   for(j in 3:ncol(df)){
     for(k in 1:nrow(df)){
