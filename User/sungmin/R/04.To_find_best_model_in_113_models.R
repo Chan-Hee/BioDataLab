1c632f9282751ee555635646f778812d88e699c7
ksmpooh <daverpooh@naver.com>
2018-04-04 11:01:56 +0900

    sm

--- User/sungmin/R/04.To_find_best_model_in_113_models.R
+++ User/sungmin/R/04.To_find_best_model_in_113_models.R
@@ -1,12 +1,12 @@
 
 for (i in 0:4){
   
-  df = read.csv(paste0("/home/tjahn/FINALMODEL/BioDataLab_ML/prediction_output/prediction_output",i,"_data.csv"),sep = ",",header = T)
+  df <- read.csv(paste0("/home/tjahn/FINALMODEL/BioDataLab_ML/prediction_output/prediction_output",i,"_data.csv"),sep = ",",header = T)
   #df_prediction = round(subset(df,select = -c(1:3)),0)
-  df = cbind(df[,2:3],round(subset(df,select = -c(1:3)),0))
+  df <- cbind(df[,2:3],round(subset(df,select = -c(1:3)),0))
   #object = nrow(df)
-  k = 0
-  j = 3
+  #k = 0
+  #j = 3
   correct = 0
   for(j in 3:ncol(df)){
     for(k in 1:nrow(df)){
@@ -16,7 +16,7 @@ for (i in 0:4){
         correct = correct + 0
       }
     }
-    df[(nrow(df)+1),j] = nrow(df)/correct
+    df[(nrow(df)+1),j] <- nrow(df)/correct
   }
   
   write.csv(paste0("/home/tjhan/Data/prediction_find_best_model/prediction_",i,".csv"))
