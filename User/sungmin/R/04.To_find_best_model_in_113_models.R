93180f0309eab498d39e2f5ac0c7ee9ec200e2e8
ksmpooh <daverpooh@naver.com>
2018-04-04 19:24:17 +0900

    sm

--- User/sungmin/R/04.To_find_best_model_in_113_models.R
+++ User/sungmin/R/04.To_find_best_model_in_113_models.R
@@ -1,15 +1,12 @@
-
 for (i in 0:4){
-  
-  df <- read.csv(paste0("/home/tjahn/FINALMODEL/BioDataLab_ML/prediction_output/prediction_output",i,"_data.csv"),sep = ",",header = T)
+  df = read.csv(paste0("D:/biodatalab/2018-1/prediction_output_each_GEO_in_each_model/prediction_output",i,"_data.csv"),sep = ",",header = T)
+  #df <- read.csv(paste0("/home/tjahn/FINALMODEL/BioDataLab_ML/prediction_output/prediction_output",i,"_data.csv"),sep = ",",header = T)
   #df_prediction = round(subset(df,select = -c(1:3)),0)
   df <- cbind(df[,2:3],round(subset(df,select = -c(1:3)),0))
-  #object = nrow(df)
-  #k = 0
-  #j = 3
   correct = 0
+  object = nrow(df)
   for(j in 3:ncol(df)){
-    for(k in 1:nrow(df)){
+    for(k in 1:object){
       if(df[k,j] == df[k,"result"]){
         correct = correct + 1
       }else{
@@ -19,8 +16,8 @@ for (i in 0:4){
     df[(nrow(df)+1),j] <- nrow(df)/correct
   }
   
-  write.csv(paste0("/home/tjhan/Data/prediction_find_best_model/prediction_",i,".csv"))
-  
+  #write.csv(paste0("/home/tjhan/Data/prediction_find_best_model/prediction_",i,".csv"))
+  write.csv(paste0("D:/biodatalab/2018-1/prediction_output_each_GEO_in_each_model/output/prediction_output",i,"_data.csv"))
 }
 
 #df = read.csv(paste0("D:/biodatalab/2018-1/prediction_output_each_GEO_in_each_model/prediction_output",i,"_data.csv"),sep = ",",header = T)
