1b17213a846796ba681d716fe10416bbb59306a4
ksmpooh <daverpooh@naver.com>
2018-04-05 21:08:30 +0900

    sm

--- User/sungmin/R/04-1.Find_best_model.R
+++ User/sungmin/R/04-1.Find_best_model.R
@@ -1,6 +1,6 @@
 Output <- as.data.frame(matrix(,nrow = 0,ncol = 3))
 column <- c("File","BestModel","Accuracy")
-colnames(Output) <- c("File","BestModel","Accuracy")
+colnames(Output) <-column
 
 for(i in 0:4){
   df <- read.csv(paste0("/home/tjahn/Data/prediction_find_best_model/prediction_",i,".csv"))
@@ -9,9 +9,9 @@ for(i in 0:4){
   df <- as.data.frame(t(df))
   max_Accuracy <- max(df[,object])
   max_row_num <- which.max(df[,object])
-  max_model <- rownames(df[max_row_num,object])
+  max_model <- rownames(df)[max_row_num]
   add <- c(paste0("prediction_",i,".csv"),max_model,max_Accuracy)
-  add <-t(matrix(add))
+  add <-as.data.frame(t(matrix(add)))
   colnames(add) <- column
   Output <- rbind(Output,add)
 }
