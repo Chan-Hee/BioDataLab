79aa99f7b59139b0881de933f107fd0e93d566d3
ksmpooh <daverpooh@naver.com>
2018-04-05 20:21:06 +0900

    sm

---
+++ User/sungmin/R/04-1.Find_best_model.R
@@ -0,0 +1,18 @@
+Output <- as.data.frame(matrix(,nrow = 0,ncol = 3))
+column <- c("File","BestModel","Accuracy")
+colnames(Output) <- c("File","BestModel","Accuracy")
+
+for(i in 0:4){
+  df <- read.csv(paste0("/home/tjahn/Data/prediction_find_best_model/prediction_",i,".csv"))
+  object <- nrow(df)
+  df <- subset(df, select = -c("result","X","patient"))
+  df <- as.data.frame(t(df))
+  max_Accuracy <- max(df[,object])
+  max_row_num <- which.max(df[,object])
+  max_model <- rownames(df[max_row_num,object])
+  add <- c(paste0("prediction_",i,".csv"),max_model,max_Accuracy)
+  add <-t(matrix(add))
+  colnames(add) <- column
+  Output <- rbind(Output,add)
+}
+write.csv(Output,"/home/tjahn/Data/prediction_find_best_model/find_best_model.csv")
--- User/sungmin/R/04.To_find_best_model_in_113.R
+++ User/sungmin/R/04.To_find_best_model_in_113.R
@@ -3,20 +3,20 @@ for (i in 0:4){
   df <- read.csv(paste0("/home/tjahn/FINALMODEL/BioDataLab_ML/prediction_output/prediction_output",i,"_data.csv"),sep = ",",header = T)
   #df_prediction = round(subset(df,select = -c(1:3)),0)
   df <- cbind(df[,2:3],round(subset(df,select = -c(1:3)),0))
-  correct = 0
-  object = nrow(df)
+  object <- 4408
   for(j in 3:ncol(df)){
+    correct <- 0
     for(k in 1:object){
       if(df[k,j] == df[k,"result"]){
-        correct = correct + 1
+        correct <- correct + 1
       }else{
-        correct = correct + 0
+        correct <- correct + 0
       }
     }
-    df[(nrow(df)+1),j] <- nrow(df)/correct
+    df[(object+1),j] <- correct/object
   }
   
-  write.csv(paste0("/home/tjhan/Data/prediction_find_best_model/prediction_",i,".csv"))
+  write.csv(df,paste0("/home/tjahn/Data/prediction_find_best_model/prediction_",i,".csv"))
   #write.csv(paste0("D:/biodatalab/2018-1/prediction_output_each_GEO_in_each_model/output/prediction_output",i,"_data.csv"))
 }
 
